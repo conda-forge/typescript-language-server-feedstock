575bfc4b59be6144894b9ff562bd38c18d0c0bd7
Daniel Nachun <daniel.nachun@gmail.com>
2025-09-29 11:06:26 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,10 +8,18 @@ jq 'del(.scripts.prepare)' package.json.bak > package.json
 
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/typescript-language-server << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/typescript-language-server/lib/cli.mjs "\$@"
+EOF
+chmod +x ${PREFIX}/bin/typescript-language-server
+
 tee ${PREFIX}/bin/typescript-language-server.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\typescript-language-server %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\typescript-language-server\lib\cli.mjs %*
 EOF
